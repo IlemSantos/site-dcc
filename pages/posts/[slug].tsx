db0703bf54e79f22c85ca8c34582c6aed80f534f
Ilem Santos <ilemlima@gmail.com>
2022-12-13 08:02:31 -0400

    News

---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,65 @@
+import { Heading, Text } from "@chakra-ui/react";
+
+import Layout from "../../components/Layout";
+import api from "../../services/api";
+
+export async function getStaticPaths() {
+  const res = await api('/posts')
+  const posts = res.data
+
+  const paths = posts.map((element: any) => ({
+    params: { slug: element.slug },
+  }))
+
+  return { paths, fallback: false }
+}
+
+export async function getStaticProps(context: any) {
+  const { params } = context
+
+  const res = await api(`/posts/${params.slug}`)
+  const post = res.data
+
+  return {
+    props: {
+      post,
+    },
+  }
+}
+
+interface PostProps {
+  post: {
+    slug: string;
+    title: string;
+    content: {
+      type?: string | undefined;
+      children: {
+        text: string;
+      }[];
+    }[];
+    type?: string | undefined;
+    id?: number | undefined;
+    status?: string | undefined;
+    link?: string | undefined;
+    author?: string | undefined;
+  }
+};
+
+export default function Post({ post }: PostProps) {
+  const { title, content, author, ...rest } = post;
+
+  return (
+    <Layout>
+      <Heading as='h3' size='lg' mb={4}>{title}</Heading>
+      {
+        content.map((element, index) => (
+          element.children.map((element, index) => (
+            <Text key={index} mb={2}>{element.text}</Text>
+          ))
+        ))
+      }
+      <Text textAlign='end'>Author: {author}</Text>
+    </Layout>
+  )
+}
+
---
+++ pages/posts/index.tsx
@@ -0,0 +1,60 @@
+import { Box, Flex, Heading, Stack, Text } from '@chakra-ui/react';
+import Link from 'next/link';
+
+import Layout from '../../components/Layout';
+import api from '../../services/api';
+
+export async function getStaticProps() {
+  const res = await api('/posts')
+  const posts = res.data
+
+  return {
+    props: {
+      posts,
+    },
+  }
+}
+
+interface NewsProps {
+  posts: {
+    slug: string;
+    title: string;
+    content: {
+      type?: string | undefined;
+      children: {
+        text: string;
+      }[];
+    }[];
+    type?: string | undefined;
+    id?: number | undefined;
+    status?: string | undefined;
+    link?: string | undefined;
+    author?: string | undefined;
+  }[]
+};
+
+export default function News({ posts }: NewsProps) {
+  return (
+    <Layout>
+      <Flex flexWrap='wrap' gap={6}>
+        {
+          posts.map((element, index) => (
+            <Stack direction='column' w='md' p={4} bg='white' borderRadius='md' key={index}>
+              <Heading as='h4' size='md' mb={2}>{element.title}</Heading>
+              <Box my={2}>
+                {
+                  element.content.map((element, index) => (
+                    element.children.map((element, index) => (
+                      <Text key={index} mb={2}>{element.text}</Text>
+                    ))
+                  ))
+                }
+              </Box>
+              <Link href={`/posts/${element.slug}`}>Ver mais</Link>
+            </Stack>
+          ))
+        }
+      </Flex>
+    </Layout>
+  )
+}
